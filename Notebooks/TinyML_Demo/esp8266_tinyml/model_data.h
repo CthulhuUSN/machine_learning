4995d3aeaea72a9e1569a32c9b93277ab468776e
CthulhuUSN <8527243+CthulhuUSN@users.noreply.github.com>
2020-12-01 16:25:23 -0600

    Moved TinyML Demo to its own repo

--- Notebooks/TinyML_Demo/esp8266_tinyml/model_data.h
+++
@@ -1,7 +0,0 @@
-#ifndef TENSORFLOW_LITE_MODEL_DATA_H_
-#define TENSORFLOW_LITE_MODEL_DATA_H_
-
-extern const unsigned char g_model_data[];
-extern const int g_model_data_len;
-
-#endif  // TENSORFLOW_LITE_MODEL_DATA_H_
